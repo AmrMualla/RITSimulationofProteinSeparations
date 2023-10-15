25c488243d955ae7b5fa0d63c5da55d4bf474581
AmrMualla <muallaamr@gmail.com>
2023-10-15 13:57:09 -0400

    frontend initial build

--- frontend/jbio-web-app/src/components/Sidebar.js
+++ frontend/jbio-web-app/src/components/Sidebar.js
@@ -2,40 +2,41 @@ import React from 'react';
 
 function Sidebar({ isOpen, toggle }) {
     return (
-        <div className={isOpen ? "sidebar" : "sidebar sidebar-collapsed"}>
-            <button className="toggle-btn" onClick={toggle}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
+        <div className="container">
+            <div className={isOpen ? "sidebar" : "sidebar sidebar-collapsed"}>
+                <button className="toggle-btn" onClick={toggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
 
+                { !isOpen && (
+                    <div className="icon-list">
+                        <span>1DE</span>
+                        <span>2DE</span>
+                        <img src="information-button.png" alt="" className="instructions-icon" />
+                        <img src="user.png" alt="" className="contact-icon" />
+                        <a href="https://github.com/AmrMualla/RITSimulationofProteinSimulations" target="_blank" rel="noopener noreferrer">
+                            <img src="github-sign.png" alt="" className="github-icon" />
+                        </a>
+                    </div>
+                )}
 
-            { !isOpen && (
-                <div className="icon-list">
-                    <span>1DE</span>
-                    <span>2DE</span>
-                    <img src="information-button.png" alt="" className="instructions-icon" />
-                    <img src="user.png" alt="" className="contact-icon" />
-                    <img src="github-sign.png" alt="" className="github-icon" />
-                </div>
-            )}
-
+                { isOpen && (
+                    <ul className="menu">
+                        <li>1D Electrophoresis</li>
+                        <li>2D Electrophoresis</li>
+                        <li>Instructions</li>
+                        <li>Contact</li>
+                        <li><a href="https://github.com/AmrMualla/RITSimulationofProteinSimulations" target="_blank" rel="noopener noreferrer">Github</a></li>
+                    </ul>
+                )}
+            </div>
             <div className="sidebar-header">
                 <span className="sidebar-title">JBioFramework</span>
             </div>
-
-            { isOpen && (
-                <ul className="menu">
-                    <li>1D Electrophoresis</li>
-                    <li>2D Electrophoresis</li>
-                    <li>Instructions</li>
-                    <li>Contact</li>
-                    <li><a href="https://github.com" target="_blank" rel="noopener noreferrer">Github</a></li>
-                </ul>
-            )}
         </div>
     );
 }
-
 
 export default Sidebar;

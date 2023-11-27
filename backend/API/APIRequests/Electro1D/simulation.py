5e01a4b6c6d3769baa201410f02070b1a51e7f42
Skeeter2600 <andersbe2600@gmail.com>
2023-11-26 20:10:54 -0500

    added standards api link

--- backend/API/APIRequests/Electro1D/simulation.py
+++ backend/API/APIRequests/Electro1D/simulation.py
@@ -1,9 +1,43 @@
+# author: Beck Anderson
+
 from fastapi import APIRouter
 
 router = APIRouter(
     prefix='/1DElectrophoresis',
     tags=['1D Simulation']
 )
+
+
+@router.get("/standards")
+async def standards():
+    """
+    This function will get the information for the
+    standards in the simulation. This currently includes the
+    names and molecular weights, but may add additional values.
+    Will never remove any. :)
+    :return: The data. Comes in a json format, but accessed easily.
+    """
+    return {'standards':
+                [{'name': 'Myosin',
+                  'molecular_weight': 200},
+                 {'name': 'β-galactosidase',
+                  'molecular_weight': 116.3},
+                 {'name': 'Phosphorylase b',
+                  'molecular_weight': 97.4},
+                 {'name': 'Bovine Serum Albumin',
+                  'molecular_weight': 66.2},
+                 {'name': 'Ovalbumin',
+                  'molecular_weight': 45},
+                 {'name': 'Carbonic Anhydrase',
+                  'molecular_weight': 31},
+                 {'name': 'Soybean Trypsin Inhibitor',
+                  'molecular_weight': 21.5},
+                 {'name': 'Lysozyme',
+                  'molecular_weight': 14.4},
+                 {'name': 'Aprotinin',
+                  'molecular_weight': 6.5}
+                 ]
+            }
 
 
 @router.get("/runInfo")

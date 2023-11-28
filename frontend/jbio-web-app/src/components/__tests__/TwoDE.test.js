a18d57eeec9f8b3e4a1f5e9038074ebb623bd9e7
cma6320 <cma6320@g.rit.edu>
2023-11-28 15:57:14 -0500

    Adding Jest test classes for each component

---
+++ frontend/jbio-web-app/src/components/__tests__/TwoDE.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // For the "toBeInTheDocument" matcher
+import TwoDE from '../TwoDE';
+
+test('renders TwoDE component', () => {
+  const { getByText, getByRole } = render(<TwoDE />);
+
+  // Check if the component renders the correct header
+  expect(getByText('2DE page')).toBeInTheDocument();
+
+});

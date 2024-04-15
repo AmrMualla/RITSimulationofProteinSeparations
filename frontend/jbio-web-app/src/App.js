3f78b8b391258efd5ce54682511848469b349a35
cma6320 <cma6320@g.rit.edu>
2024-04-15 14:13:09 -0400

    Line error

--- frontend/jbio-web-app/src/App.js
+++ frontend/jbio-web-app/src/App.js
@@ -15,7 +15,6 @@ function App() {
     return (
         <div className="app-container" data-theme={isDark ? "dark" : "light"}>
             <Sidebar isOpen={isSidebarOpen} toggle={() => setIsSidebarOpen(!isSidebarOpen)} isDark={isDark} />
-            <Sidebar isOpen={isSidebarOpen} isDark={isDark} toggle={() => setIsSidebarOpen(!isSidebarOpen)} />
             <Router isOpen={isSidebarOpen} />
             <DarkToggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
         </div>
